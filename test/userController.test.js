a9b4db54c4b8327fa8a6a45adc7d1fdbf29c718a
Heidi Peralta <heidi.peralta@gmail.com>
2018-08-20 14:58:14 -0400

    User login test cases

--- test/userController.test.js
+++ test/userController.test.js
@@ -13,22 +13,22 @@ const user = {
 
 before( async () => {
   await mongoose.connect(process.env.DB);
-  await User.remove({email: 'valid@email.com'});
+  await User.remove({ email: 'valid@email.com' });
 });
 
 after(done => done());
 
-describe("User Resitration", () => {
+describe("User Registration", () => {
 
   const registerCall = payload => {
     return request(process.env.BASE_URL)
-      .post('/auth/register')
+      .post('/register')
       .send(payload)
       .set('Accept', 'application/json');
   }
 
   it("Should ask for name", (done) => {
-    registerCall({ email: 'test@email.com' })
+    registerCall({ email: 'test@email.com', password: 'abc123' })
       .expect( response => {
         if (!response.body.messages.includes('Name is required')) {
           throw new Error('Missing name validation');
@@ -46,7 +46,7 @@ describe("User Resitration", () => {
   });
 
   it("Should ask for valid email", (done) => {
-    registerCall({ email: 'invalid', name: 'valid name'})
+    registerCall({ email: 'invalid', name: 'valid name' })
       .expect( response => {
         if (!response.body.messages.includes('Please provide a valid Email Address')) {
           throw new Error('Missing email format validation');
@@ -55,16 +55,25 @@ describe("User Resitration", () => {
   });
 
   it("Should ask for a password", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name'})
+    registerCall({ email: 'valid@email.com', name: 'valid name' })
       .expect( response => {
         if (!response.body.messages.includes('Password is required')) {
           throw new Error('Missing password validation');
         }
       }).end(done);
   });
+
+  // it("Should ask for a valid password", (done) => {
+    // registerCall({ email: 'valid@email.com', name: 'valid name', password: '<script>alert(malicious!)</script>' })
+    //   .expect( response => {
+    //     if (!response.body.messages.includes('Password can only contain letters, numbers and the following: @!&*()')) {
+    //       throw new Error('Missing password validation');
+    //     }
+    //   }).end(done);
+  // });
   
   it("Password should at least be 6 characters long", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name', password: '12345'})
+    registerCall({ email: 'valid@email.com', name: 'valid name', password: '12345' })
       .expect( response => {
         if (!response.body.messages.includes('Password should at least be 6 characters long')) {
           throw new Error('Missing password length validation');
@@ -73,7 +82,7 @@ describe("User Resitration", () => {
   });
   
   it("Should ask for a password confirmation", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123'})
+    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123' })
       .expect( response => {
         if (!response.body.messages.includes('A Password Confirmation must be provided')) {
           throw new Error('Missing password confirmation validation');
@@ -82,7 +91,7 @@ describe("User Resitration", () => {
   });
 
   it("Password and Password Confirmation should match", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: '123'})
+    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: '123' })
       .expect( response => {
         
         if (!response.body.messages.includes('The Password and Confirmation do not match')) {
@@ -92,19 +101,75 @@ describe("User Resitration", () => {
   });
 
   it("Should register the user!", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: 'abc123'})
+    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: 'abc123' })
       .expect( response => {
-        if (response.status !== 200) {
-          throw new Error('The User failed to register');
+        if (response.statusCode !== 302) {
+          throw new Error('User registration test failed');
         }
       }).end(done);
   });
 
   it("Email should be unique", (done) => {
-    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: 'abc123'})
+    registerCall({ email: 'valid@email.com', name: 'valid name', password: 'abc123', passwordConfirmation: 'abc123' })
       .expect( response => {
         if (!response.body.messages.includes('An account with this email already exists, forgot password?')) {
           throw new Error('Unique email validation fails');
+        }
+      }).end(done);
+  });
+
+});
+
+describe("User Login", () => {
+
+  loginCall = payload => {
+    return request(process.env.BASE_URL)
+      .post('/login')
+      .send(payload)
+      .set('Accept', 'application/json');
+  }
+
+  it("Should ask for email", (done) => {
+    loginCall({ password: 'test' })
+      .expect( response => {
+        if (!response.body.messages.includes('Email cannot be blank')) {
+          throw new Error('Missing email validation');
+        }
+      }).end(done);
+  });
+
+  it("Should ask for valid email", (done) => {
+    loginCall({ email: 'invalid', password: 'test' })
+      .expect( response => {
+        if (!response.body.messages.includes('Please provide a valid Email Address')) {
+          throw new Error('Missing email format validation');
+        }
+      }).end(done);
+  });
+
+  it("Should ask for a password", (done) => {
+    loginCall({ email: 'valid@email.com' })
+      .expect( response => {
+        if (!response.body.messages.includes('Password is required')) {
+          throw new Error('Missing password validation');
+        }
+      }).end(done);
+  });
+  
+  // it("Should ask for a valid password", (done) => {
+    // loginCall({ email: 'valid@email.com', password: '<script>alert(malicious!)</script>' })
+    //   .expect( response => {
+    //     if (!response.body.messages.includes('Password can only contain letters, numbers and the following: @!&*()')) {
+    //       throw new Error('Missing password validation');
+    //     }
+    //   }).end(done);
+  // });
+
+  it("Should log the user in!", (done) => {
+    loginCall({ email: 'valid@email.com', password: 'abc123' })
+      .expect( response => {
+        if (response.status !== 200) {
+          throw new Error('User login did not work');
         }
       }).end(done);
   });
